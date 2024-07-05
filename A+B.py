773152a420c6b587d35dde858df592b51e7762b5
junho <dlwnsgh0530@naver.com>
2024-07-05 11:09:10 +0900

    값 입력 받고, 출력하기

--- A+B.py
+++ A+B.py
@@ -1,10 +1,10 @@
-#int A 이렇게 쓰는거 아님 
-#int B
+#int A 이거 아님?
+#파이썬은 변수 선언을 int a로 하지 않음 / 이 친구는 int(input("~~")) 이렇게 함
+
+#A+B 값 입력받아 출력하기
 
 A=int(input("값 입력 : "))
+
 B=int(input("값 입력 : "))
-
-A>0,B<10
-
 
 print(A+B)
