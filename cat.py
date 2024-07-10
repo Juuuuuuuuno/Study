b5b4ee67046fb99916df7b3bfb2ed2543b03567f
junho <dlwnsgh0530@naver.com>
2024-07-10 10:50:30 +0900

    애옹이출력

---
+++ cat.py
@@ -0,0 +1,10 @@
+# 고양이 출력
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+      
+# 왜 밑줄이 나와서 사람을 불안하게 만드는가?
+# print("\    /\")  < 여 뒤가 문제란다.              SyntaxError: EOL while scanning string literal
+# python은 " \ "를 출력하려면 " \\ "로 사용해야 " \ "나옴
